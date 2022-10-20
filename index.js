f1951e7c4fae287aac907a96009014ab0884220b
Isaac Tellez <isaactellez@Isaacs-MacBook-Pro.local>
2022-10-20 08:18:45 -0600

    table almost done

--- index.js
+++ index.js
@@ -111,35 +111,64 @@ function addList() {
  async function displayTeebox() {
   let getYardsLoop =  await getinfo()
   let dropDownValue = document.getElementById('tee-box-select').value;
-  let holeRow = '<th scope="row" >Row'
+  let holeRow = '<th scope="row" >Hole'
+  let splitHoleRow = getYardsLoop.data.holes.length / 2
   let yards = '<th scope="row">Yards</th>';
   let par = '<th scope="row">Par</th>';
   let handicap = '<th scope="row">Handicap</th>'
-  
-  
-  for(let i = 1 ; i <= getYardsLoop.data.holes.length; i++) {
-     holeRow += `<th>${i}</th>`
+  let yardsTotalCount = 0;
+  let parTotalCount = 0;
+  let handicapTotalCount = 0;
+  let totalTotalCount = 0;
+  let outTotalCount = 0;
+
+  for(let i = 1; i <= splitHoleRow; i++) {
+    holeRow += `<th <td >${i}</th>`
+
+  }
+
+  holeRow += `<th>Out</th>`
+
+  for(let i = splitHoleRow + 1 ; i <= getYardsLoop.data.holes.length; i++) {
+     holeRow += `<th <td >${i}</th>`
  }
+ holeRow += `<th>In</th>`
+ holeRow += `<th>Total</th`
+ 
   document.querySelector('table').innerHTML = holeRow
 
   for(let i = 0; i < getYardsLoop.data.holes.length; i++) {
     let getYards = getYardsLoop.data.holes[i].teeBoxes[dropDownValue].yards;
-
+    yardsTotalCount += getYards
+    totalTotalCount = yardsTotalCount;
     yards += ` <td>${getYards}</td>`
 
   }
+  yards += `<td>${yardsTotalCount}</td>`
+  yards += `<td>${totalTotalCount}</td>`
   for(let i = 0; i < getYardsLoop.data.holes.length; i++) {
     let getPar = getYardsLoop.data.holes[i].teeBoxes[dropDownValue].par;
-
+    parTotalCount += getPar
+    totalTotalCount = parTotalCount;
     par += ` <td>${getPar}</td>`
 
   }
+  par += `<td>${parTotalCount}</td>`
+  par += `<td>${totalTotalCount}</td>`
   for(let i = 0; i < getYardsLoop.data.holes.length; i++) {
     let getHandicap = getYardsLoop.data.holes[i].teeBoxes[dropDownValue].hcp;
-
+    handicapTotalCount += getHandicap
+    totalTotalCount = handicapTotalCount;
     handicap += ` <td>${getHandicap}</td>`
 
   }
+  handicap += `<td>${handicapTotalCount}</td>`
+  handicap += `<td>${totalTotalCount}</td>`
+
+  for(let i = 1; i >= splitHoleRow.length; i++) {
+    
+  }
+
   document.querySelector('table').innerHTML += yards;
   yards += '</tr>'
 
@@ -148,7 +177,6 @@ function addList() {
 
   document.querySelector('table').innerHTML += handicap;
   handicap += '</tr>'
-
 
  }
 
@@ -177,12 +205,12 @@ let addPlayerCounter = 0;
   let namePlayer = `<tr> <th onclick="removePlayer(${addGuy.id})" scope="row">${addGuy.name}</th>`
   let getYardsLoop =  await getinfo()
   playerArray.push(addGuy);
-  
 
   for(let i = 0; i < getYardsLoop.data.holes.length; i++) {
     namePlayer += `<td data-bs-toggle="modal" data-bs-target="#exampleModal" onclick="changeScoreData(${i}, ${addGuy.id}, ${addPlayerCounter})" id="${addGuy.id}playerBox${i}"></td>`
-
+   
   }
+  namePlayer += `<td id="player-score-${addGuy.id}">0</td>`
   document.querySelector('table').innerHTML += namePlayer;
   namePlayer += '</tr>'
   addPlayerCounter += 1;
@@ -209,9 +237,31 @@ let addPlayerCounter = 0;
   whenChanged = indexNumber;
   otherVariable = guyId;
   playerCounter = otherPlayerCounter;
+
   console.log(whenChanged, otherVariable, playerCounter)
  }
 
+
+ function updateTotalPlayScore(guyId) {
+  const scoreTotalElement = document.querySelector(`#player-score-${guyId}`);
+  const totalPlayerScore = getTotalPlayerScore(guyId);
+
+  scoreTotalElement.textContent = totalPlayerScore;
+ }
+
+ function getTotalPlayerScore(guyId) {
+  const playerScoreElements = Array.from(document.querySelectorAll(`[id^="${guyId}playerBox"]`))
+  let totalPlayerScore = 0;
+
+  for(let i =0; i < playerScoreElements.length; i++) {
+    const scoreElementItem = playerScoreElements[i]
+    const score = Number(scoreElementItem.textContent || 0)
+    totalPlayerScore += score;
+  }
+
+  return totalPlayerScore
+
+ }
 
 
 
